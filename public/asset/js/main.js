504e03af02148249f866edc2f50e60574cf1e5a1
rageQuitt <thesecretiveplotter@gmail.com>
2023-07-21 17:53:56 +0100

    1ercommit

---
+++ public/asset/js/main.js
@@ -0,0 +1,32 @@
+'use strict'
+console.log('main.js is loaded');
+
+//Formulaire drapeau
+
+var input = document.querySelector("#phone-number");
+window.intlTelInput(input, {
+    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
+});
+
+//Captcha
+async function validateCaptcha(captchaResponse) {
+    const response = await fetch('https://hcaptcha.com/siteverify', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            'response': captchaResponse,  // the 'h-captcha-response' value
+            'secret': '0xFd90F054A632Ce6582863330732b1dbAB61c5903'
+        })
+    });
+
+    const captchaValidation = await response.json();
+    if (!captchaValidation.success) {
+        // handle the failed validation
+        console.error("Captcha validation failed");
+    }
+}
+
+// You would call validateCaptcha with the captcha response value when the form is submitted
+// validateCaptcha(someCaptchaResponseValue);
